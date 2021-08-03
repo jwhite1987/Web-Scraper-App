69f93d5a1d4ee9b66ff15e7a0826eef7a3aca96e
Jimmy <>
2021-08-03 16:05:12 -0400

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -8,18 +8,6 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb+srv://jimmywhite87:Ruger2012@cluster0.caep8.mongodb.net/missionstomars?retryWrites=true&w=majority")
 
-
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", vacation=mars_data)
-
-
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
@@ -32,6 +20,18 @@ def scrape():
 
     # Redirect back to home page
     return redirect("/")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", vacation=mars_data)
+
+
 
 @app.route("/images")
 def images():
